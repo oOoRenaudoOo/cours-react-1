99983ab5a1c8edd870c4d186d085a8d6c797a275
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-08-10 17:50:11 -0500

    React components selectedar - FIN

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Card = ({ country }) => {
-  console.log(country);
   return (
     <li className="card">
+      <img
+        src={country.flags.svg}
+        alt={"drapeau " + country.translations.fra.common}
+      />
       <div className="infos">
-        <h2>Nom du pays</h2>
+        <h2>{country.translations.fra.common}</h2>
+        <h4>{country.capital}</h4>
+        <p>Pop. {country.population.toLocaleString()}</p>
       </div>
     </li>
   );
